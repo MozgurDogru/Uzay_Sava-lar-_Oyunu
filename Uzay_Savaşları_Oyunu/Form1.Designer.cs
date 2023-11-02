d15313cc15b622bd49d94d65b6a7fa373913629c
ozgur <ozgurr_dogru@hotmail.com>
2023-11-02 13:14:33 +0300

    Add project files.

---
+++ Uzay_Savaşları_Oyunu/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace Uzay_Savaşları_Oyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Galaxy = new System.Windows.Forms.PictureBox();
+            this.OyuncuGemisi = new System.Windows.Forms.PictureBox();
+            this.LabelPuan = new System.Windows.Forms.Label();
+            this.timerOyuncu = new System.Windows.Forms.Timer(this.components);
+            this.timerMermiFirlat = new System.Windows.Forms.Timer(this.components);
+            this.timerDusmanDusur = new System.Windows.Forms.Timer(this.components);
+            this.timerDusmanOlustur = new System.Windows.Forms.Timer(this.components);
+            this.timerMermiKontrol = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Galaxy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OyuncuGemisi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Galaxy
+            // 
+            this.Galaxy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Galaxy.Image = global::Uzay_Savaşları_Oyunu.Properties.Resources.Evren_04__1_;
+            this.Galaxy.Location = new System.Drawing.Point(0, 0);
+            this.Galaxy.Name = "Galaxy";
+            this.Galaxy.Size = new System.Drawing.Size(472, 588);
+            this.Galaxy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Galaxy.TabIndex = 0;
+            this.Galaxy.TabStop = false;
+            this.Galaxy.Click += new System.EventHandler(this.Galaxy_Click);
+            // 
+            // OyuncuGemisi
+            // 
+            this.OyuncuGemisi.BackColor = System.Drawing.Color.Transparent;
+            this.OyuncuGemisi.Image = global::Uzay_Savaşları_Oyunu.Properties.Resources.Gemi_01;
+            this.OyuncuGemisi.Location = new System.Drawing.Point(184, 419);
+            this.OyuncuGemisi.Name = "OyuncuGemisi";
+            this.OyuncuGemisi.Size = new System.Drawing.Size(92, 69);
+            this.OyuncuGemisi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.OyuncuGemisi.TabIndex = 1;
+            this.OyuncuGemisi.TabStop = false;
+            // 
+            // LabelPuan
+            // 
+            this.LabelPuan.AutoSize = true;
+            this.LabelPuan.BackColor = System.Drawing.Color.Transparent;
+            this.LabelPuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LabelPuan.ForeColor = System.Drawing.Color.White;
+            this.LabelPuan.Location = new System.Drawing.Point(12, 9);
+            this.LabelPuan.Name = "LabelPuan";
+            this.LabelPuan.Size = new System.Drawing.Size(85, 24);
+            this.LabelPuan.TabIndex = 2;
+            this.LabelPuan.Text = "Puan = 0";
+            // 
+            // timerOyuncu
+            // 
+            this.timerOyuncu.Interval = 2;
+            this.timerOyuncu.Tick += new System.EventHandler(this.timerOyuncu_Tick);
+            // 
+            // timerMermiFirlat
+            // 
+            this.timerMermiFirlat.Interval = 10;
+            this.timerMermiFirlat.Tick += new System.EventHandler(this.timerMermiFirlat_Tick);
+            // 
+            // timerDusmanDusur
+            // 
+            this.timerDusmanDusur.Interval = 50;
+            this.timerDusmanDusur.Tick += new System.EventHandler(this.timerDusmanDusur_Tick);
+            // 
+            // timerDusmanOlustur
+            // 
+            this.timerDusmanOlustur.Interval = 5000;
+            this.timerDusmanOlustur.Tick += new System.EventHandler(this.timerDusmanOlustur_Tick);
+            // 
+            // timerMermiKontrol
+            // 
+            this.timerMermiKontrol.Tick += new System.EventHandler(this.timerMermiKontrol_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 588);
+            this.Controls.Add(this.LabelPuan);
+            this.Controls.Add(this.OyuncuGemisi);
+            this.Controls.Add(this.Galaxy);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Galaxy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OyuncuGemisi)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Galaxy;
+        private System.Windows.Forms.PictureBox OyuncuGemisi;
+        private System.Windows.Forms.Label LabelPuan;
+        private System.Windows.Forms.Timer timerOyuncu;
+        private System.Windows.Forms.Timer timerMermiFirlat;
+        private System.Windows.Forms.Timer timerDusmanDusur;
+        private System.Windows.Forms.Timer timerDusmanOlustur;
+        private System.Windows.Forms.Timer timerMermiKontrol;
+    }
+}
+
---
+++ Uzay_Savaşları_Oyunu/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Uzay_Savaşları_Oyunu
+{
+    public partial class Form1 : Form
+    {
+        int formYukseklik = 700, formGenislik = 500;
+        int Oyuncu_X = 200, Oyuncu_Y = 400;//Oyuncu koordinatlar
+        int OyuncuHiz_Yatay =0, OyuncuHiz_Dusey = 0;
+        int Puan ;
+
+        List<PictureBox> Mermiler = new List<PictureBox>();
+
+        int MermiHiz = 20;
+
+        List<PictureBox> Dusmanlar = new List<PictureBox>();
+        int DusmanHiz = 2;
+        Random rnd= new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timerOyuncu_Tick(object sender, EventArgs e)
+        {
+            Oyuncu_X += OyuncuHiz_Yatay;
+            Oyuncu_Y += OyuncuHiz_Dusey;
+            OyuncuGemisi.Location = new Point(Oyuncu_X, Oyuncu_Y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Height = formYukseklik;
+            this.Width= formGenislik;
+            //Objelerimizi Galaxy içine alıyoruz.
+            OyuncuGemisi.Parent = Galaxy;
+            LabelPuan.Parent= Galaxy;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int hiz = 2;
+            switch (e.KeyCode)
+            {
+                case Keys.Left:OyuncuHiz_Yatay -= hiz;
+                    break;
+                case Keys.Right:OyuncuHiz_Yatay += hiz;
+                    break;
+                case Keys.Up:OyuncuHiz_Dusey   -= hiz;
+                    break;
+                case Keys.Down:OyuncuHiz_Dusey+= hiz;
+                    break;                
+
+                    case Keys.Enter:
+                    Puan = 0;
+                    timerOyuncu.Start();
+                    timerDusmanOlustur.Start();
+                    timerDusmanDusur.Start();
+                    timerMermiFirlat.Start();
+                    timerMermiKontrol.Start();
+                    break;
+
+                    case Keys.Space:
+                    MermiOlustur();
+                    break;
+            }
+
+        }
+
+        private void timerMermiFirlat_Tick(object sender, EventArgs e)
+        {
+            MermileriFirlat();
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    OyuncuHiz_Yatay = 0;
+                    break;
+                case Keys.Right:
+                    OyuncuHiz_Yatay = 0;
+                    break;
+                case Keys.Up:
+                    OyuncuHiz_Dusey = 0;
+                    break;
+                case Keys.Down:
+                    OyuncuHiz_Dusey = 0;
+                    break;
+            }
+
+        }
+
+        public void MermiOlustur() 
+        {
+            PictureBox Mermi = new PictureBox
+            {
+                Size = new Size(15, 15),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.Laser_08,
+                BackColor = Color.Transparent
+            };
+            int MermiLocX = OyuncuGemisi.Left + OyuncuGemisi.Width/ 2 - 5;
+            int MermiLocY = OyuncuGemisi.Top - 1 ;
+            Mermi.Location = new Point(MermiLocX, MermiLocY );
+            Controls.Add( Mermi );
+            Mermi.Parent = Galaxy;
+            Mermi.BringToFront(); //Galaxy resminin üstüne çıkartıyoruz.
+            Mermiler.Add( Mermi );
+        }
+
+        private void timerDusmanDusur_Tick(object sender, EventArgs e)
+        {
+            DusmanDusur();
+        }
+
+        private void timerDusmanOlustur_Tick(object sender, EventArgs e)
+        {
+            DusmanOlustur();
+        }
+
+        private void timerMermiKontrol_Tick(object sender, EventArgs e)
+        {
+            for (int m = 0; m < Mermiler.Count; m++)
+            {
+                for (int d = 0; d < Dusmanlar.Count; d++)
+                {
+                    //mermi ile düşman çarpıştı mı?
+                    if (Mermiler[m].Bounds.IntersectsWith(Dusmanlar[d].Bounds))
+                    {
+                        Puan += 1;
+                        LabelPuan.Text = "Puan=" +Puan.ToString();
+
+                        Galaxy.Controls.Remove(Mermiler[m]);
+                        Mermiler.Remove(Mermiler[m]);
+
+                        Galaxy.Controls.Remove(Dusmanlar[d]);
+                        Dusmanlar.Remove(Dusmanlar[d]);
+
+                        //RAM hafıza temizleme
+                        GC.Collect();//Çöp toplayıcısı
+                        GC.WaitForPendingFinalizers();//Çöpleri yok et
+                    }
+                }
+            }
+        }
+
+        public void MermileriFirlat() 
+        {
+            for (int i = 0; i < Mermiler.Count; i++)
+            {
+                Mermiler[i].Top -= MermiHiz;
+            }
+        }
+
+        private void Galaxy_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void DusmanOlustur() 
+        {
+            int yer = rnd.Next(0,formGenislik - 50);
+            PictureBox Dusman = new PictureBox
+            {
+                Size = new Size(50, 50),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Image = Properties.Resources.Gemi_03,
+                BackColor = Color.Transparent,
+                Location = new Point(yer, 0),
+                Visible = true
+            };
+            Controls.Add( Dusman );// forma ekledik
+            Dusman.Parent= Galaxy;// Galaxy içine aldık
+            Dusman.BringToFront();// öne getirdik
+            Dusmanlar.Add(Dusman);
+        }
+
+        public void DusmanDusur() 
+        {
+            for (int i = 0; i < Dusmanlar.Count; i++)
+            {
+                Dusmanlar[i].Top += DusmanHiz;
+                //Düşman gemisi formun dışına çıkarsa
+                //Oyuncunun gemisine çarparsa
+                if (Dusmanlar[i].Top >= formYukseklik - 10 || Dusmanlar[i].Bounds.IntersectsWith(OyuncuGemisi.Bounds))
+                {
+                    //oyun bitti
+                    OyunBitir();
+                }
+            }
+        }
+
+
+        public void OyunBitir() 
+        {
+            timerOyuncu.Stop();
+            timerMermiFirlat.Stop();
+            timerDusmanDusur.Stop();
+            timerMermiKontrol.Stop();
+            timerDusmanOlustur.Stop();
+
+            MessageBox.Show("Oyun Bitti,İşinin başına...!");
+        }
+    }
+
+
+}
